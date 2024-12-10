b366b0033ce88aacba464228dbd8e21d5b551689
blessian <blessian.dev@gmail.com>
2024-12-10 18:57:50 +0900

    fix frontend

--- weerobot/frontend/dashboard/src/App.jsx
+++ weerobot/frontend/dashboard/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
--- weerobot/frontend/dashboard/src/hooks/useStore.js
+++ weerobot/frontend/dashboard/src/hooks/useStore.js
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand';  // 수정된 부분
 
 export const useStore = create((set) => ({
   videoModalOpen: false,
